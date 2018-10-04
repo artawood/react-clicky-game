67a39e9daa26ca234d2356a9b9310c387c9a4729
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-10-04 12:57:01 -0700

    added characters

--- src/components/Main/Card/Card.js
+++ src/components/Main/Card/Card.js
@@ -2,27 +2,30 @@ import React from "react";
 import "./Card.css";
 
 const Card = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
+  <div className="col-4">
+    <div className="card" draggable="true">
+        <div className="img-container">
+        <img alt={props.name} src={props.image} />
+        </div>
+        {/* <div className="content">
+        <ul>
+            <li>
+            <strong>Name:</strong> {props.name}
+            </li>
+            <li>
+            <strong>Occupation:</strong> {props.occupation}
+            </li>
+            <li>
+            <strong>Location:</strong> {props.location}
+            </li>
+        </ul>
+        </div> */}
+        {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+        </span> */}
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
-      𝘅
-    </span> */}
-  </div>
+  </div>  
+  
 );
 
 export default Card;
